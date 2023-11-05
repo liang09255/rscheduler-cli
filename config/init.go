5bdaa31e9aafa62f3b16bc088ad506e57456339b
梁晓生 <liang09255@gmail.com>
2023-11-05 16:18:49 +0800

    refactor: remove config.yaml

--- config/init.go
+++ config/init.go
@@ -1,19 +1,5 @@
 package config
 
-import (
-	"github.com/spf13/viper"
-)
-
 func Init() {
-	v := viper.New()
-	v.SetConfigName("config")
-	v.AddConfigPath("./config")
-	err := v.ReadInConfig()
-	if err != nil {
-		panic("read config failed, err: " + err.Error())
-	}
-	err = v.Unmarshal(&Config)
-	if err != nil {
-		panic("unmarshal config failed, err: " + err.Error())
-	}
+	Config.BaseURL = "http://localhost:8080"
 }
